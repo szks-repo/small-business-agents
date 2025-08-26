dc0f18afd2262c274cecb32b69655dbe464b94f4
szks-repo <szkscookie@gmail.com>
2025-08-26 21:42:42 +0900

    add: webhookProcessor

--- app/cmd/webhook_processor.go
+++ app/cmd/webhook_processor.go
@@ -1,1 +1,68 @@
 package cmd
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+var webhookProcessorCmd = &cobra.Command{
+	Use: "webhookProcessor",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := cmd.Context()
+
+		if err := godotenv.Load("../.env"); err != nil {
+			slog.Info("No .env file found, using environment variables")
+		}
+
+		endpoint := os.Getenv("SQS_ENDPOINT")
+		region := os.Getenv("AWS_REGION")
+		if region == "" {
+			region = "ap-northeast-1"
+		}
+
+		cfg, err := config.LoadDefaultConfig(ctx,
+			config.WithRegion(region),
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		sqsClient := sqs.NewFromConfig(cfg, func(o *sqs.Options) {
+			o.BaseEndpoint = aws.String(endpoint)
+		})
+
+		queueUrl, err := sqsClient.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+			QueueName: aws.String("webhook-event-queue"),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			received, err := sqsClient.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+				QueueUrl:            queueUrl.QueueUrl,
+				MaxNumberOfMessages: 20,
+				VisibilityTimeout:   300,
+				WaitTimeSeconds:     20,
+			})
+			if err != nil {
+				slog.Error("Failed to ReceiveMessage", "error", err)
+				return
+			}
+
+			for _, msg := range received.Messages {
+				slog.Info("Receive message", "messageId", msg.MessageId)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(webhookProcessorCmd)
+}
