819f8959c0097b88d3aec07a9ba0e72a8fc0dc99
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-26 22:21:40 +0900

    add: createTestEvent command

---
+++ app/cmd/create_test_event.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+)
+
+var createTestEventPath string
+var createTestEventMessage string
+
+var createTestEventCmd = &cobra.Command{
+	Use: "createTestEvent",
+	Run: func(cmd *cobra.Command, args []string) {
+		slog.Info("createTestEvent start")
+
+		ctx := cmd.Context()
+
+		_, file, _, _ := runtime.Caller(0)
+		envPath := filepath.Join(file, "../../../.env")
+		if err := godotenv.Load(envPath); err != nil {
+			slog.Info("No .env file found, using environment variables")
+		}
+
+		payload := lo.Must(json.Marshal(map[string]any{
+			"ts":      time.Now(),
+			"message": createTestEventMessage,
+		}))
+
+		webhookUrl := lo.Must(url.JoinPath("http://localhost"+os.Getenv("WEBHOOK_RECEIVER_ADDR"), "webhook", createTestEventPath))
+		req, err := http.NewRequestWithContext(ctx, "POST", webhookUrl, bytes.NewReader(payload))
+		if err != nil {
+			panic(err)
+		}
+		client := http.DefaultClient
+		res, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			slog.Error("Failed to send webhook", "webhookUrl", webhookUrl, "statusCode", res.StatusCode)
+			return
+		}
+
+		slog.Info("Success to send webhook", "statusCode", res.StatusCode, "payload", string(payload))
+	},
+}
+
+func init() {
+	createTestEventCmd.Flags().StringVar(&createTestEventPath, "p", "/contact", "")
+	createTestEventCmd.Flags().StringVar(&createTestEventMessage, "m", "my message", "")
+	rootCmd.AddCommand(createTestEventCmd)
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.31.2
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.42.0
 	github.com/joho/godotenv v1.5.1
+	github.com/samber/lo v1.51.0
 	github.com/spf13/cobra v1.9.1
 )
 
@@ -24,4 +25,5 @@ require (
 	github.com/aws/smithy-go v1.22.5 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
