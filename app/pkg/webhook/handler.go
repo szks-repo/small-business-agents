9abbb25f7ca6f0293affecb716210b2f55ed44be
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-27 22:05:41 +0900

    add: handlers

--- app/pkg/webhook/handler.go
+++ app/pkg/webhook/handler.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 
 	"github.com/szks-repo/small-business-agents/app/app/pkg/types"
+	"github.com/szks-repo/small-business-agents/app/app/pkg/webhook/events"
 )
 
 type WebhookHandler interface {
@@ -21,11 +22,28 @@ func NewHandler() WebhookHandler {
 func (h *webhookHandler) Handle(ctx context.Context, payload *types.WebhookPayload) error {
 	switch WebhookPathToKind[payload.Path] {
 	case WebhookKindContactReceived:
-		//
+		return h.handleContactReceived(ctx, payload.Body)
 	case WebhookKindEmailReceived:
-		//
+		return h.handleContactReceived(ctx, payload.Body)
 	default:
 		slog.Info("Unknown webhook kind", "path", payload.Path)
+		return nil
+	}
+}
+
+func (h *webhookHandler) handleContactReceived(ctx context.Context, body []byte) error {
+	var e events.ContactReceived
+	if err := e.Unmarshal(body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *webhookHandler) handleEmailReceived(ctx context.Context, body []byte) error {
+	var e events.EmailReceived
+	if err := e.Unmarshal(body); err != nil {
+		return err
 	}
 
 	return nil
