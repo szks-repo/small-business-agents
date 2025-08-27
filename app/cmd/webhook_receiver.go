539f8a75da0ac1659dd56edaacb94bcacc4201a1
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-27 21:34:12 +0900

    tidy: init

--- app/cmd/webhook_receiver.go
+++ app/cmd/webhook_receiver.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"github.com/szks-repo/small-business-agents/app/app/pkg/types"
 )
@@ -22,21 +23,15 @@ var webhookReceiverCmd = &cobra.Command{
 		endpoint := os.Getenv("SQS_ENDPOINT")
 		region := os.Getenv("AWS_REGION")
 
-		cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
-		if err != nil {
-			panic(err)
-		}
+		cfg := lo.Must(config.LoadDefaultConfig(ctx, config.WithRegion(region)))
 
 		sqsClient := sqs.NewFromConfig(cfg, func(o *sqs.Options) {
 			o.BaseEndpoint = aws.String(endpoint)
 		})
 
-		queueUrl, err := sqsClient.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+		queueUrl := lo.Must(sqsClient.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
 			QueueName: aws.String("webhook-event-queue"),
-		})
-		if err != nil {
-			panic(err)
-		}
+		}))
 
 		mux := http.NewServeMux()
 		mux.HandleFunc("POST /webhook/", func(w http.ResponseWriter, r *http.Request) {
