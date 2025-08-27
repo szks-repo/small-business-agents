e8c3f1ce5d6f8b9b4f8762e51f4955b9e2f191e6
szks-repo <szkscookie@gmail.com>
2025-08-27 22:18:27 +0900

    add: Unmarshal

--- app/pkg/webhook/events/email.go
+++ app/pkg/webhook/events/email.go
@@ -1,13 +1,20 @@
 package events
 
+import "encoding/json"
+
 type EmailReceived struct {
-	From   string
-	To     string
-	CC     []string
-	Header string
-	Body   string
+	From   string   `json:"from"`
+	To     string   `json:"to"`
+	CC     []string `json:"cc"`
+	Header string   `json:"header"`
+	Body   string   `json:"body"`
 }
 
 func (p *EmailReceived) Unmarshal(data []byte) error {
+	//TBD
+	var dst EmailReceived
+	json.Unmarshal(data, &dst)
+	p = &dst
+
 	return nil
 }
