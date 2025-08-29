14309fa71a1010e7d7f85335a6941a4026e34a9c
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-29 20:29:18 +0900

    tidy

--- app/pkg/mail/mail.go
+++ app/pkg/mail/mail.go
@@ -1,11 +1,9 @@
 package mailllib
 
 import (
-	"fmt"
 	"mime"
 	"net/mail"
 )
-
 
 func ParseFROM(from string) (*mail.Address, error) {
 	// RFC 2047形式でエンコードされた文字列をデコード
@@ -15,13 +13,10 @@ func ParseFROM(from string) (*mail.Address, error) {
 		return nil, err
 	}
 
-	// デコードした文字列をパースして、送信者名とメールアドレスを取得
 	addr, err := mail.ParseAddress(decodedFrom)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("Name: %s\n", addr.Name)
-	fmt.Printf("Address: %s\n", addr.Address)
 	return addr, nil
 }
